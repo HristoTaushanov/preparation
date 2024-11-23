20be5a2cb8d7ec96025cb611b1c2fc11fc5f7aed
Hristo Taushanov <htaushanov@yahoo.com>
2024-11-23 12:01:23 +0200

    created JASON method for the REST API

--- lib/preparation-stack.ts
+++ lib/preparation-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -16,7 +18,15 @@ export class PreparationStack extends cdk.Stack {
       billingMode: BillingMode.PAY_PER_REQUEST
     });
 
+
+    const processFunction = new NodejsFunction(this, 'processFunction', {
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'handler',
+      entry: '${__dirname}/../src/processFunction.ts'
+    });
+
     const api = new RestApi(this, 'ProcessorApi');
     const resource = api.root.addResource('processJASON');
+    resource.addMethod('POST', new LambdaIntegration(processFunction));
   }
 }
