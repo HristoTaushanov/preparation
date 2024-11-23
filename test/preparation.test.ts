e32dcee76c6dabd121023ca87ea13dfa11f65d13
Hristo Taushanov <htaushanov@yahoo.com>
2024-11-24 00:31:50 +0200

    npm install --save-dev jest-cdk-snapshot

--- test/preparation.test.ts
+++ test/preparation.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Preparation from '../lib/preparation-stack';
+ import * as cdk from 'aws-cdk-lib';
+ import { Template } from 'aws-cdk-lib/assertions';
+ import * as Preparation from '../lib/preparation-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/preparation-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Preparation.PreparationStack(app, 'MyTestStack');
+   const stack = new Preparation.PreparationStack(app, 'MyTestStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
+   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+   template.hasResourceProperties('AWS::SQS::Queue', {
+     VisibilityTimeout: 300
+   });
 });
